64b39bff12f8f30dbc8397b4308407ee5ef1ce1d
James Garfield <jordan@grupojvm.com>
2022-04-22 11:09:37 -0500

    Implemented bounce

--- Scripts/Enemies/Base.cs
+++ Scripts/Enemies/Base.cs
@@ -1,48 +1,33 @@
 using Godot;
 using System;
-using CharacterNode;
+using Character;
 
 namespace Enemy
 {
-    public class Base : Character
+    public class Base : Character.Node
     {
-        private RayCast2D leftCast = null;
-        private RayCast2D rightCast = null;
-
         public override void _Ready()
         {
-            // Get the RayCasts
-            leftCast = GetNode<RayCast2D>("RayCast2D/Left"); // TODO: Not sure about these names.
-            rightCast = GetNode<RayCast2D>("RayCast2D/Right"); // TODO: Not sure about these names.
+            speed = 100;
+            // Randomly choose a direction to move in
+            Random rand = new Random();
+            if (rand.Next(2) == 0)
+            {
+                speed *= -1;
+            }
         }
 
         protected override void PhysicsProcess(float delta)
         {
             base.PhysicsProcess(delta);
-            BounceOff(delta);
-        }
-
-        /// <summary>
-        /// The enemy bounces off walls and Enemies.
-        /// </summary>
-        private void BounceOff(float delta)
-        {
-            // Check which cast is colliding
-            int direction = 0;
-
-            if (leftCast.IsColliding())
+            
+            // Check if the enemy is on a wall
+            if (IsOnWall())
             {
-                direction = -1;
-            }
-            else if (rightCast.IsColliding())
-            {
-                direction = 1;
+                speed = -speed;
             }
 
-            // Maybe we are not colliding with anything?
-            if (direction == 0) return;
-
-            MoveH(direction * speed, delta);
+            MoveH(speed, delta);
         }
     }
 }
